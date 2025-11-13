5a8a9942027c55e72364f1ac48fdfa8c35fec70d
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-11-13 21:32:20 +0530

    new prettier config

--- lua/custom/plugins/prettier.lua
+++ lua/custom/plugins/prettier.lua
@@ -1,3 +1,18 @@
-return {
-  'MunifTanjim/prettier.nvim',
+local status, prettier = pcall(require, 'prettier')
+if not status then
+  return
+end
+
+prettier.setup {
+  bin = 'prettierd',
+  filetypes = {
+    'css',
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+    'json',
+    'scss',
+    'less',
+  },
 }
