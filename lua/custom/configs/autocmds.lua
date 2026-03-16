64bc600d181c9222c00232cc41c2949acdee941f
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-03-16 19:45:47 +0530

    Some small changes

--- lua/custom/configs/autocmds.lua
+++ lua/custom/configs/autocmds.lua
@@ -4,10 +4,3 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     vim.lsp.buf.format { async = false }
   end,
 })
-
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    vim.cmd "lua require('snacks').toggle.inlay_hints():toggle()"
-    vim.cmd "lua require('snacks').toggle.indent():toggle()"
-  end,
-})
