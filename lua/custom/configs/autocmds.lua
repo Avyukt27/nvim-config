d16f9b347ce6f99befff2d55b8e4afe6fc0afc11
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-04-08 18:59:20 +0530

    Added plugin for ca65, edited bufferline and file explorer behaviour

--- lua/custom/configs/autocmds.lua
+++ lua/custom/configs/autocmds.lua
@@ -4,3 +4,16 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     vim.lsp.buf.format { async = false }
   end,
 })
+
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = '*/NES/**/*.s',
+  callback = function()
+    vim.bo.filetype = 'asm_ca65'
+  end,
+})
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    Snacks.explorer()
+  end,
+})
--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -6,8 +6,14 @@ return {
   },
   config = function()
     require('bufferline').setup {}
-    vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>bp', ':bprev<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>bn', function()
+      Snacks.bufnext()
+    end, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>bp', function()
+      Snacks.bufprev()
+    end, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>bd', function()
+      Snacks.bufdelete()
+    end, { noremap = true, silent = true })
   end,
 }
